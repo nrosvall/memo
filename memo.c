32272eae07d70ee099a86ee48d2b4356e1d85308
Niko Rosvall <niko@byteptr.com>
2019-06-12 22:48:15 +0300

    Update copyright year

--- memo.c
+++ memo.c
@@ -1,6 +1,6 @@
 /* Memo is a Unix-style note-taking software.
  *
- * Copyright (C) 2014-2015 Niko Rosvall <niko@byteptr.com>
+ * Copyright (C) 2014-2019 Niko Rosvall <niko@byteptr.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
